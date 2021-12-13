1a770937018169de09db91f7dd564cb6770e828a
Fernando P Franco <fernandopessoafranco@hotmail.com>
2021-12-13 05:16:34 +0000

    init version.

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@ServletComponentScan
+@SpringBootApplication(scanBasePackages = "com.example.demo")
+@EntityScan("com.example.demo")
+@EnableJpaRepositories("com.example.demo")
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
